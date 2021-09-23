4ed281488905678a166bd0689b7cec4e9c2ad627
mattmallow <matthewenzo@icloud.com>
2021-09-24 08:22:37 +1200

    Added typical

--- src/pages/HomePage/Home.js
+++ src/pages/HomePage/Home.js
@@ -1,15 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Home.css";
+import Typical from "react-typical";
 
 function Home() {
   return (
     <div id="home-div">
-      <p className="one-second-animation" id="hi-there">
+      <p className="paragraph one-second-animation" id="hi-there">
         Hiya, I am
       </p>
       <h1 className="h1 three-second-animation">Matthew Fan</h1>
-      <h2 className="h2 five-second-animation">I am a Software Developer</h2>
+      <h2 className="h2 five-second-animation">
+        I am a{" "}
+        <Typical
+          loop={Infinity}
+          wrapper="b"
+          steps={["developer", 2000, "Student", 2000, "Gamer", 2000]}
+        />
+      </h2>
       <Link to="/cv">
         <p className="paragraph seven-second-animation" id="intro">
           I have experience in Python, React.JS, Java, SQL...
